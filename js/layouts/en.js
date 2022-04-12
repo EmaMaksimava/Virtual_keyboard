1ecd14e48beda3b18f9e55de79a1fe2d14e2141e
Emma Maksimava <cielvibritania@gmail.com>
2022-04-13 00:14:41 +0200

    feat: add ru and en files

---
+++ js/layouts/en.js
@@ -0,0 +1,322 @@
+export default [
+  {
+    standart: '`',
+    shift: '~',
+    code: 'Backquote',
+  },
+  {
+    standart: '1',
+    shift: '!',
+    code: 'Digit1',
+  },
+  {
+    standart: '2',
+    shift: '@',
+    code: 'Digit2',
+  },
+  {
+    standart: '3',
+    shift: '#',
+    code: 'Digit3',
+  },
+  {
+    standart: '4',
+    shift: '$',
+    code: 'Digit4',
+  },
+  {
+    standart: '5',
+    shift: '%',
+    code: 'Digit5',
+  },
+  {
+    standart: '6',
+    shift: '^',
+    code: 'Digit6',
+  },
+  {
+    standart: '7',
+    shift: '&',
+    code: 'Digit7',
+  },
+  {
+    standart: '8',
+    shift: '*',
+    code: 'Digit8',
+  },
+  {
+    standart: '9',
+    shift: '(',
+    code: 'Digit9',
+  },
+  {
+    standart: '0',
+    shift: ')',
+    code: 'Digit0',
+  },
+  {
+    standart: '-',
+    shift: '_',
+    code: 'Minus',
+  },
+  {
+    standart: '=',
+    shift: '+',
+    code: 'Equal',
+  },
+  {
+    standart: 'Backspace',
+    shift: null,
+    code: 'Backspace',
+  },
+  {
+    standart: 'Tab',
+    shift: null,
+    code: 'Tab',
+  },
+  {
+    standart: 'q',
+    shift: 'Q',
+    code: 'KeyQ',
+  },
+  {
+    standart: 'w',
+    shift: 'W',
+    code: 'KeyW',
+  },
+  {
+    standart: 'e',
+    shift: 'E',
+    code: 'KeyE',
+  },
+  {
+    standart: 'r',
+    shift: 'R',
+    code: 'KeyR',
+  },
+  {
+    standart: 't',
+    shift: 'T',
+    code: 'KeyT',
+  },
+  {
+    standart: 'y',
+    shift: 'Y',
+    code: 'KeyY',
+  },
+  {
+    standart: 'u',
+    shift: 'U',
+    code: 'KeyU',
+  },
+  {
+    standart: 'i',
+    shift: 'I',
+    code: 'KeyI',
+  },
+  {
+    standart: 'o',
+    shift: 'O',
+    code: 'KeyO',
+  },
+  {
+    standart: 'p',
+    shift: 'P',
+    code: 'KeyP',
+  },
+  {
+    standart: 'a',
+    shift: 'A',
+    code: 'KeyA',
+  },
+  {
+    standart: 's',
+    shift: 'S',
+    code: 'KeyS',
+  },
+  {
+    standart: 'd',
+    shift: 'D',
+    code: 'KeyD',
+  },
+  {
+    standart: 'f',
+    shift: 'F',
+    code: 'KeyF',
+  },
+  {
+    standart: 'g',
+    shift: 'G',
+    code: 'KeyG',
+  },
+  {
+    standart: 'h',
+    shift: 'H',
+    code: 'KeyH',
+  },
+  {
+    standart: 'j',
+    shift: 'J',
+    code: 'KeyJ',
+  },
+  {
+    standart: 'k',
+    shift: 'K',
+    code: 'KeyK',
+  },
+  {
+    standart: 'l',
+    shift: 'L',
+    code: 'KeyL',
+  },
+  {
+    standart: 'z',
+    shift: 'Z',
+    code: 'KeyZ',
+  },
+  {
+    standart: 'x',
+    shift: 'X',
+    code: 'KeyX',
+  },
+  {
+    standart: 'c',
+    shift: 'C',
+    code: 'KeyC',
+  },
+  {
+    standart: 'v',
+    shift: 'V',
+    code: 'KeyV',
+  },
+  {
+    standart: 'b',
+    shift: 'B',
+    code: 'KeyB',
+  },
+  {
+    standart: 'n',
+    shift: 'N',
+    code: 'KeyN',
+  },
+  {
+    standart: 'm',
+    shift: 'M',
+    code: 'KeyM',
+  },
+  {
+    standart: ';',
+    shift: ':',
+    code: 'Semicolon',
+  },
+  {
+    standart: '\'',
+    shift: '"',
+    code: 'Quote',
+  },
+  {
+    standart: '[',
+    shift: '{',
+    code: 'BracketLeft',
+  },
+  {
+    standart: ']',
+    shift: '}',
+    code: 'BracketRight',
+  },
+  {
+    standart: '\\',
+    shift: '|',
+    code: 'Backslash',
+  },
+  {
+    standart: ',',
+    shift: '<',
+    code: 'Comma',
+  },
+  {
+    standart: '.',
+    shift: '>',
+    code: 'Period',
+  },
+  {
+    standart: '/',
+    shift: '?',
+    code: 'Slash',
+  },
+  {
+    standart: 'CapsLock',
+    shift: null,
+    code: 'CapsLock',
+  },
+  {
+    standart: 'Shift',
+    shift: null,
+    code: 'ShiftLeft',
+  },
+  {
+    standart: 'Enter',
+    shift: null,
+    code: 'Enter',
+  },
+  {
+    standart: 'Delete',
+    shift: null,
+    code: 'NumpadDecimal',
+  },
+  {
+    standart: 'Shift',
+    shift: null,
+    code: 'ShiftRight',
+  },
+  {
+    standart: 'ArrowLeft',
+    shift: null,
+    code: 'ArrowLeft',
+  },
+  {
+    standart: 'ArrowUp',
+    shift: null,
+    code: 'ArrowUp',
+  },
+  {
+    standart: 'ArrowRight',
+    shift: null,
+    code: 'ArrowRight',
+  },
+  {
+    standart: 'ArrowDown',
+    shift: null,
+    code: 'ArrowDown',
+  },
+  {
+    standart: 'Control',
+    shift: null,
+    code: 'ControlRight',
+  },
+  {
+    standart: 'Alt',
+    shift: null,
+    code: 'AltRight',
+  },
+  {
+    standart: ' ',
+    shift: null,
+    code: 'Space',
+  },
+  {
+    standart: 'Alt',
+    shift: null,
+    code: 'AltLeft',
+  },
+  {
+    standart: 'Meta',
+    shift: null,
+    code: 'MetaLeft',
+  },
+  {
+    standart: 'Control',
+    shift: null,
+    code: 'ControlLeft',
+  },
+];
---
+++ js/layouts/ru.js
@@ -0,0 +1,322 @@
+export default [
+  {
+    standart: 'ё',
+    shift: 'Ё',
+    code: 'Backquote',
+  },
+  {
+    standart: '1',
+    shift: '!',
+    code: 'Digit1',
+  },
+  {
+    standart: '2',
+    shift: '"',
+    code: 'Digit2',
+  },
+  {
+    standart: '3',
+    shift: '№',
+    code: 'Digit3',
+  },
+  {
+    standart: '4',
+    shift: ';',
+    code: 'Digit4',
+  },
+  {
+    standart: '5',
+    shift: '%',
+    code: 'Digit5',
+  },
+  {
+    standart: '6',
+    shift: ':',
+    code: 'Digit6',
+  },
+  {
+    standart: '7',
+    shift: '?',
+    code: 'Digit7',
+  },
+  {
+    standart: '8',
+    shift: '*',
+    code: 'Digit8',
+  },
+  {
+    standart: '9',
+    shift: '(',
+    code: 'Digit9',
+  },
+  {
+    standart: '0',
+    shift: ')',
+    code: 'Digit0',
+  },
+  {
+    standart: '-',
+    shift: '_',
+    code: 'Minus',
+  },
+  {
+    standart: '=',
+    shift: '+',
+    code: 'Equal',
+  },
+  {
+    standart: 'Backspace',
+    shift: null,
+    code: 'Backspace',
+  },
+  {
+    standart: 'Tab',
+    shift: null,
+    code: 'Tab',
+  },
+  {
+    standart: 'й',
+    shift: 'Й',
+    code: 'KeyQ',
+  },
+  {
+    standart: 'ц',
+    shift: 'Ц',
+    code: 'KeyW',
+  },
+  {
+    standart: 'у',
+    shift: 'У',
+    code: 'KeyE',
+  },
+  {
+    standart: 'к',
+    shift: 'К',
+    code: 'KeyR',
+  },
+  {
+    standart: 'е',
+    shift: 'Е',
+    code: 'KeyT',
+  },
+  {
+    standart: 'н',
+    shift: 'Н',
+    code: 'KeyY',
+  },
+  {
+    standart: 'г',
+    shift: 'Г',
+    code: 'KeyU',
+  },
+  {
+    standart: 'ш',
+    shift: 'Ш',
+    code: 'KeyI',
+  },
+  {
+    standart: 'щ',
+    shift: 'Щ',
+    code: 'KeyO',
+  },
+  {
+    standart: 'з',
+    shift: 'З',
+    code: 'KeyP',
+  },
+  {
+    standart: 'ф',
+    shift: 'Ф',
+    code: 'KeyA',
+  },
+  {
+    standart: 'ы',
+    shift: 'Ы',
+    code: 'KeyS',
+  },
+  {
+    standart: 'в',
+    shift: 'В',
+    code: 'KeyD',
+  },
+  {
+    standart: 'а',
+    shift: 'А',
+    code: 'KeyF',
+  },
+  {
+    standart: 'п',
+    shift: 'П',
+    code: 'KeyG',
+  },
+  {
+    standart: 'р',
+    shift: 'Р',
+    code: 'KeyH',
+  },
+  {
+    standart: 'о',
+    shift: 'О',
+    code: 'KeyJ',
+  },
+  {
+    standart: 'л',
+    shift: 'Л',
+    code: 'KeyK',
+  },
+  {
+    standart: 'д',
+    shift: 'Д',
+    code: 'KeyL',
+  },
+  {
+    standart: 'я',
+    shift: 'Я',
+    code: 'KeyZ',
+  },
+  {
+    standart: 'ч',
+    shift: 'Ч',
+    code: 'KeyX',
+  },
+  {
+    standart: 'с',
+    shift: 'С',
+    code: 'KeyC',
+  },
+  {
+    standart: 'м',
+    shift: 'М',
+    code: 'KeyV',
+  },
+  {
+    standart: 'и',
+    shift: 'И',
+    code: 'KeyB',
+  },
+  {
+    standart: 'т',
+    shift: 'Т',
+    code: 'KeyN',
+  },
+  {
+    standart: 'ь',
+    shift: 'Ь',
+    code: 'KeyM',
+  },
+  {
+    standart: 'ж',
+    shift: 'Ж',
+    code: 'Semicolon',
+  },
+  {
+    standart: 'э',
+    shift: 'Э',
+    code: 'Quote',
+  },
+  {
+    standart: 'х',
+    shift: 'Х',
+    code: 'BracketLeft',
+  },
+  {
+    standart: 'ъ',
+    shift: 'Ъ',
+    code: 'BracketRight',
+  },
+  {
+    standart: '\\',
+    shift: '|',
+    code: 'Backslash',
+  },
+  {
+    standart: 'б',
+    shift: 'Б',
+    code: 'Comma',
+  },
+  {
+    standart: 'ю',
+    shift: 'Ю',
+    code: 'Period',
+  },
+  {
+    standart: '.',
+    shift: ',',
+    code: 'Slash',
+  },
+  {
+    standart: 'CapsLock',
+    shift: null,
+    code: 'CapsLock',
+  },
+  {
+    standart: 'Shift',
+    shift: null,
+    code: 'ShiftLeft',
+  },
+  {
+    standart: 'Enter',
+    shift: null,
+    code: 'Enter',
+  },
+  {
+    standart: 'Delete',
+    shift: null,
+    code: 'NumpadDecimal',
+  },
+  {
+    standart: 'Shift',
+    shift: null,
+    code: 'ShiftRight',
+  },
+  {
+    standart: 'ArrowLeft',
+    shift: null,
+    code: 'ArrowLeft',
+  },
+  {
+    standart: 'ArrowUp',
+    shift: null,
+    code: 'ArrowUp',
+  },
+  {
+    standart: 'ArrowRight',
+    shift: null,
+    code: 'ArrowRight',
+  },
+  {
+    standart: 'ArrowDown',
+    shift: null,
+    code: 'ArrowDown',
+  },
+  {
+    standart: 'Control',
+    shift: null,
+    code: 'ControlRight',
+  },
+  {
+    standart: 'Alt',
+    shift: null,
+    code: 'AltRight',
+  },
+  {
+    standart: ' ',
+    shift: null,
+    code: 'Space',
+  },
+  {
+    standart: 'Alt',
+    shift: null,
+    code: 'AltLeft',
+  },
+  {
+    standart: 'Meta',
+    shift: null,
+    code: 'MetaLeft',
+  },
+  {
+    standart: 'Control',
+    shift: null,
+    code: 'ControlLeft',
+  },
+];
